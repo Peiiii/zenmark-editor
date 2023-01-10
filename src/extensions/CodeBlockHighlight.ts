f37426e8b559e186b36c9327c162802df8c868cd
Wang Pei <1535376447@qq.com>
2023-01-11 00:43:22 +0800

    update something

--- src/extensions/CodeBlockHighlight.ts
+++ src/extensions/CodeBlockHighlight.ts
@@ -4,6 +4,10 @@ import css from 'highlight.js/lib/languages/css'
 import js from 'highlight.js/lib/languages/javascript'
 import ts from 'highlight.js/lib/languages/typescript'
 import html from 'highlight.js/lib/languages/xml'
+import python from 'highlight.js/lib/languages/python'
+import shell from 'highlight.js/lib/languages/shell'
+import clang from 'highlight.js/lib/languages/c'
+import cplusplus from 'highlight.js/lib/languages/cpp';
 // load all highlight.js languages
 import { lowlight } from 'lowlight'
 
@@ -12,7 +16,16 @@ import CodeBlockComponent from './CodeBlockComponent'
 lowlight.registerLanguage('html', html)
 lowlight.registerLanguage('css', css)
 lowlight.registerLanguage('js', js)
+lowlight.registerLanguage('javascript', js)
 lowlight.registerLanguage('ts', ts)
+lowlight.registerLanguage('typescript', ts)
+lowlight.registerLanguage('py', python)
+lowlight.registerLanguage('python', python)
+lowlight.registerLanguage('c', clang)
+lowlight.registerLanguage('cpp', cplusplus)
+lowlight.registerLanguage('sh', shell) 
+lowlight.registerLanguage('shell', shell) 
+lowlight.registerLanguage('bash', shell) 
 
 export const CodeBlockHighlight = CodeBlockLowlight
     .extend({
