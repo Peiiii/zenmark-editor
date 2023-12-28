6cba4217198cb8a6f091c5f9a4fbe72c6b9f01cf
peiiii <1535376447@qq.com>
2023-12-28 18:17:06 +0800

    'update'

---
+++ bean.tsx
@@ -0,0 +1,10 @@
+import { defineBean } from "@/toolkit/factories/bean";
+
+export const EditorBean = defineBean(
+  () => ({}),
+  () => {
+    const readContent = () => {};
+    const writeContent = () => {};
+    return {  };
+  }
+);
