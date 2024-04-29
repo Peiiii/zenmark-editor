6692fa122a9b6b443b879cdf04c4e9aa264ef527
peiiii <1535376447@qq.com>
2024-04-29 21:15:35 +0800

    'update'

--- src/toolkit/factories/rpc/protocal/communicationChannel.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export interface ICommunicationChannel {
-  sendMessage<T>(message: T): Promise<void>;
-  onMessage<T>(callback: (message: T) => void): void;
-}
